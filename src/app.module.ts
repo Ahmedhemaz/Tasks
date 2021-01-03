23bcee436f0f899f4058b141f988930dfe33fae8
Ahmedhemaz <ah_ibrahim94@hotmail.com>
2021-01-03 06:33:19 +0200

    Create task-type-subdomain

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ScheduleSubdomainModule } from './schedule-subdomain/schedule-subdomain.module';
+import { TaskTypeSubdomainModule } from './task-type-subdomain/task-type-subdomain.module';
 
 @Module({
-  imports: [ScheduleSubdomainModule],
+  imports: [ScheduleSubdomainModule, TaskTypeSubdomainModule],
   controllers: [AppController],
   providers: [AppService],
 })
