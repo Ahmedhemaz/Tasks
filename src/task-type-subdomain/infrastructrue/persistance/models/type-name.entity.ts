8303bae8d7a37f5297c36dee0047c3f9082eea37
Ahmedhemaz <ah_ibrahim94@hotmail.com>
2021-01-04 07:19:45 +0200

    Create taskTypeDataModel

---
+++ src/task-type-subdomain/infrastructrue/persistance/models/type-name.entity.ts
@@ -0,0 +1,18 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity({ name: 'types' })
+export class taskTypeDataModel {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @CreateDateColumn({ name: 'created_at' })
+    createdAt: Date;
+
+    @UpdateDateColumn({ name: 'updated_at' })
+    updatedAt: Date;
+
+}
