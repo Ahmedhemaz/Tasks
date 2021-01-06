bdd8a42aeec8e9c84fb1e1ed6a17e1e12dd40400
Ahmedhemaz <ah_ibrahim94@hotmail.com>
2021-01-06 11:48:35 +0200

    Create image-url value-object

--- src/task-type-subdomain/domain/error-messages/errors.ts
+++ src/task-type-subdomain/domain/error-messages/errors.ts
@@ -1,5 +1,6 @@
 const EMPTY_NAME_ERROR = 'Type name can\'t be empty';
-
+const EMPTY_IMAGE_URL_ERROR = 'Image URL can\'t be empty';
 export {
-    EMPTY_NAME_ERROR
+    EMPTY_NAME_ERROR,
+    EMPTY_IMAGE_URL_ERROR,
 }
---
+++ src/task-type-subdomain/domain/value-objects/image-url.ts
@@ -0,0 +1,22 @@
+import isEmpty from "validator/lib/isEmpty";
+import { IValueObject } from "../../../shared-kernal/interfaces/IValueObject";
+import { EMPTY_IMAGE_URL_ERROR } from "../error-messages/errors";
+import { EmptyStringException } from "../exceptions/emptyString.exception";
+
+export class ImageURL implements IValueObject<ImageURL> {
+    private readonly url: string;
+
+    constructor(url: string) {
+
+        if (isEmpty(url, { ignore_whitespace: true })) throw new EmptyStringException(EMPTY_IMAGE_URL_ERROR);
+        this.url = url;
+    }
+    equals(imageURL: ImageURL): boolean {
+        return imageURL.getImageURL() === this.url;
+    }
+
+    public getImageURL(): Readonly<string> {
+        return this.url;
+    }
+
+}
